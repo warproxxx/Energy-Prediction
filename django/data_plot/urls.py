3999637cc09477740b541b25befafeee7d17857d
warproxxx <Daniel Deployment Server>
2018-08-21 03:00:23 +0000

    Updated to reflect django update

--- django/data_plot/urls.py
+++ django/data_plot/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, re_path
 from . import views
 from django.conf.urls import url, include
-from django.contrib.auth.views import login
+from django.contrib.auth import login
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
