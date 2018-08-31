b4292393ea1cf4498d1623bb8c46efdac67089db
Daniel Sapkota <daniel@quantorithm.com>
2018-08-31 07:35:54 +0545

    Performed a lot of backtests

--- run_default_backtest.py
+++ run_default_backtest.py
@@ -1,6 +1,9 @@
 from data_plot.bt import perform_backtest
 from glob import glob
 import os
+from algorithm.basic_utils import get_logger
+
+logger = get_logger('algorithm/logs/backtest.log')
 
 test_type = 'backtest'
 starting_cash = 10000
@@ -15,4 +18,5 @@ for model in glob('algorithm/models/*'):
         city = os.path.basename(city)
 
         for strategy in strategies:
+            logger.info("Performing backtest for {}/{}/{}".format(model, city, strategy))
             _, _, _, _, _, _ = perform_backtest(city, model, test_type, starting_cash, comission, strategy)
